c3baf7eed18fa37218373f932381e4f95256b724
Chiranthana14 <52007079+Chiranthana14@users.noreply.github.com>
2024-02-13 12:54:12 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,15 +1,17 @@
 // Your First C++ Program
 
 #include <iostream>
+using namespace std;
 
 int main() {
     int n;
     
     cout << "enter an  number: ";
-    cin << n;
+    cin >> n;
     if(n%2==0)
         cout <<n<<"is even";
     else
         cout<<n<<"is odd";
+    
     return 0;
 }
