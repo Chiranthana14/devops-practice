5d1afd5ea27e8bed4922a1a7579f704a022c11ae
Chiranthana14 <52007079+Chiranthana14@users.noreply.github.com>
2024-02-18 21:51:22 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 // Your First C++ Program
-
 #include <iostream>
-using namespace std;
 
-int main(int argc, char** argv) {
-    std::cout << "Hello World" << std::endl;
-    return 0;
+int main(int argc, char** argv){
+	std::cout << "Hello World" << std::endl;
+	return 0;
 }
