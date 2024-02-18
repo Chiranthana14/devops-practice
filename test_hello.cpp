d4ef46e0d472ffefb51e9d0c3444dc243e36d2df
Chiranthana14 <52007079+Chiranthana14@users.noreply.github.com>
2024-02-18 23:49:36 +0530

    Update test_hello.cpp

--- test_hello.cpp
+++ test_hello.cpp
@@ -5,3 +5,8 @@
 TEST(HelloTest, HelloWorldOutput) {
     EXPECT_EQ("Hello, world!", hello());
 }
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
